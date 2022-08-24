c87add09ed7eafcabd8b988a29adad8fa856a535
okada genya <g.okada.wilf+github@gmail.com>
2022-08-24 09:58:21 +0900

    wip

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+export const urlList = [
+  {
+    name: 'あらBfm',
+    url: 'https://anchor.fm/s/319a2820/podcast/rss'
+  },
+  {
+    name: 'メディアヌップ',
+    url: 'hthttps://anchor.fm/s/51c8e3c/podcast/rss'
+  },
+  {
+    name: 'rebuild',
+    url: 'https://feeds.rebuild.fm/rebuildfm'
+  }
+]
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+/**
+ * 1. configファイルの取得
+ * 1. URLリストの書き出し　rss/urllist.json
+ * 1. github actionファイルの書き出し .github/workflows/blank.yml
+ */
+import {urlList} from './config'
+
+
+// URLリストの書き出し　rss/urllist.json
+// github actionファイルの書き出し .github/workflows/blank.yml
