1c006982d7da5e080758bcf8f318ef914c5b02b0
okada genya <g.okada.wilf+github@gmail.com>
2022-08-29 09:42:42 +0900

    json変換をpromiseで一括実行するように修正

--- src/translate.ts
+++ src/translate.ts
@@ -2,38 +2,44 @@ import { getEncodedUrl } from "./config.ts";
 import { writePodcastJSON } from "./write.ts";
 import * as xml from "https://deno.land/x/xml@2.0.4/mod.ts";
 
-// create a SAX parser instance
+const output = async (item) => {
+  // xmlの読み込み
+  const file = await Deno.readTextFile(`rss/${item.hashEncoded}.xml`);
+
+  console.log(file);
+
+  // xml to json
+  const jsObj = xml.parse(file, { debug: false });
+
+  // json の書き出し
+
+  let json = jsObj?.rss?.channel || "";
+  // airsap の動的な要素を削除する
+  if (item.url.includes(`airsap.net`)) {
+    json.item = json.item.map((d) => {
+      delete d["content:encoded"];
+
+      return d;
+    });
+  } else if (
+    [`anchor.fm`, `pitpa.jp`].some((pattern) => item.url.includes(pattern))
+  ) {
+    delete json["lastBuildDate"];
+  }
+
+  console.log(json);
+  await writePodcastJSON(item.hashEncoded, json);
+};
 
 const main = async () => {
   const urlList = getEncodedUrl();
+  const writePromiseList = [];
   for (var i = urlList.length - 1; i >= 0; i--) {
     const item = urlList[i];
-
-    console.log(item.hashEncoded);
-
-    // xmlの読み込み
-    const file = await Deno.readTextFile(`rss/${item.hashEncoded}.xml`);
-
-    // xml to json
-    const jsObj = xml.parse(file, { debug: false });
-
-    // json の書き出し
-
-    let json = jsObj?.rss?.channel || "";
-    // airsap の動的な要素を削除する
-    if (item.url.includes(`airsap.net`)) {
-      json.item = json.item.map((d) => {
-        delete d["content:encoded"];
-
-        return d;
-      });
-    } else if (
-      [`anchor.fm`, `pitpa.jp`].some((pattern) => item.url.includes(pattern))
-    ) {
-      delete json["lastBuildDate"];
-    }
-    writePodcastJSON(item.hashEncoded, json);
+    writePromiseList.push(output(item));
   }
+
+  await Promise.allSettled(writePromiseList);
 };
 
 main();
