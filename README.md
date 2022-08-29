5c5454fda823818aee8eb93e57e4c3decad1f108
okada genya <g.okada.wilf+github@gmail.com>
2022-08-29 09:42:53 +0900

    build

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 deno run --allow-write src/main.ts
 
+deno run --allow-net --allow-write src/curlIgnoredDownload.ts
+
 sh local/downloadxml.sh
 
 deno run --allow-write --allow-read src/translate.ts
--- local/downloadxml.sh
+++ local/downloadxml.sh
@@ -1,140 +1,204 @@
           # あらBfm
+          echo 'https://anchor.fm/s/319a2820/podcast/rss'
           curl https://anchor.fm/s/319a2820/podcast/rss --compressed > rss/arkbfm.xml
 
           # メディアヌップ
+          echo 'https://anchor.fm/s/51c8e3c/podcast/rss'
           curl https://anchor.fm/s/51c8e3c/podcast/rss --compressed > rss/%E3%83%A1%E3%83%87%E3%82%A3%E3%82%A2%E3%83%8C%E3%83%83%E3%83%97.xml
 
           # rebuild
+          echo 'https://feeds.rebuild.fm/rebuildfm'
           curl https://feeds.rebuild.fm/rebuildfm --compressed > rss/rebuildfm.xml
 
           # 朝日新聞　ニュースの現場から
+          echo 'https://www.omnycontent.com/d/playlist/1e3bd144-9b57-451a-93cf-ac0e00e74446/50382bb4-3af3-4250-8ddc-ac0f0033ceb5/684015f9-2396-4ac4-bc1f-ac0f0033d08c/podcast.rss'
           curl https://www.omnycontent.com/d/playlist/1e3bd144-9b57-451a-93cf-ac0e00e74446/50382bb4-3af3-4250-8ddc-ac0f0033ceb5/684015f9-2396-4ac4-bc1f-ac0f0033d08c/podcast.rss --compressed > rss/%E3%83%8B%E3%83%A5%E3%83%BC%E3%82%B9%E3%81%AE%E7%8F%BE%E5%A0%B4%E3%81%8B%E3%82%89.xml
 
           # Repeat Rhyme / リピートライム
+          echo 'https://anchor.fm/s/9d4c028c/podcast/rss'
           curl https://anchor.fm/s/9d4c028c/podcast/rss --compressed > rss/RepeatRhyme.xml
 
           # 青いTシャツ24時　-シン現代農業論-
+          echo 'https://anchor.fm/s/1c76584c/podcast/rss'
           curl https://anchor.fm/s/1c76584c/podcast/rss --compressed > rss/%E9%9D%92T24%E6%99%82.xml
 
           # ほっとテック
+          echo 'https://anchor.fm/s/86693634/podcast/rss'
           curl https://anchor.fm/s/86693634/podcast/rss --compressed > rss/%E3%81%BB%E3%81%A3%E3%81%A8%E3%83%86%E3%83%83%E3%82%AF.xml
 
           # Zero Topic - ゼロトピック -
+          echo 'https://anchor.fm/s/1617b040/podcast/rss'
           curl https://anchor.fm/s/1617b040/podcast/rss --compressed > rss/%E3%82%BC%E3%83%AD%E3%83%88%E3%83%94.xml
 
           # オールデイアメリカ
+          echo 'https://anchor.fm/s/88052e44/podcast/rss'
           curl https://anchor.fm/s/88052e44/podcast/rss --compressed > rss/%E4%B8%80%E6%97%A5%E4%B8%89%E5%96%84.xml
 
           # ひとくちPKI
+          echo 'https://anchor.fm/s/42b89b8c/podcast/rss'
           curl https://anchor.fm/s/42b89b8c/podcast/rss --compressed > rss/%E3%81%B2%E3%81%A8%E3%81%8F%E3%81%A1PKI.xml
 
           # furoshiki.fm
+          echo 'https://anchor.fm/s/50bf64f4/podcast/rss'
           curl https://anchor.fm/s/50bf64f4/podcast/rss --compressed > rss/furoshiki_fm.xml
 
           # 歴史を面白く学ぶコテンラジオ （COTEN RADIO）
+          echo 'https://anchor.fm/s/8c2088c/podcast/rss'
           curl https://anchor.fm/s/8c2088c/podcast/rss --compressed > rss/COTENRADIO.xml
 
           # ハッピートークバラエティ「よかった探し」
+          echo 'https://anchor.fm/s/426cb1f4/podcast/rss'
           curl https://anchor.fm/s/426cb1f4/podcast/rss --compressed > rss/%E3%82%88%E3%81%8B%E3%81%A3%E3%81%9F%E6%8E%A2%E3%81%97.xml
 
           # 超相対性理論
+          echo 'https://anchor.fm/s/57a9101c/podcast/rss'
           curl https://anchor.fm/s/57a9101c/podcast/rss --compressed > rss/%E8%B6%85%E7%9B%B8%E5%AF%BE%E6%80%A7%E7%90%86%E8%AB%96.xml
 
           # エンジニアと人生
+          echo 'https://pitpa.jp/rss/shu223'
           curl https://pitpa.jp/rss/shu223 --compressed > rss/%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2%E3%81%A8%E4%BA%BA%E7%94%9F.xml
 
           # Wantedly Engineering Podcast
+          echo 'https://anchor.fm/s/64205bfc/podcast/rss'
           curl https://anchor.fm/s/64205bfc/podcast/rss --compressed > rss/Wantedly_Engineering_Podcast.xml
 
           # ALL STAR SAAS PODCAST
+          echo 'https://anchor.fm/s/4960aa9c/podcast/rss'
           curl https://anchor.fm/s/4960aa9c/podcast/rss --compressed > rss/#allstarsaas.xml
 
           # Basuke's Tech Talk
+          echo 'https://anchor.fm/s/4cf7e15c/podcast/rss'
           curl https://anchor.fm/s/4cf7e15c/podcast/rss --compressed > rss/basuke.xml
 
           # 火曜日のおフロ
+          echo 'https://anchor.fm/s/2b3dd74c/podcast/rss'
           curl https://anchor.fm/s/2b3dd74c/podcast/rss --compressed > rss/%E7%81%AB%E6%9B%9C%E6%97%A5%E3%81%AE%E3%81%8A%E3%83%95%E3%83%AD.xml
 
           # イシュミラジオ
+          echo 'https://feed.podbean.com/isyumi/feed.xml'
           curl https://feed.podbean.com/isyumi/feed.xml --compressed > rss/%E3%82%A4%E3%82%B7%E3%83%A5%E3%83%9F%E3%83%A9%E3%82%B8%E3%82%AA.xml
 
           # たまにデザインFM
+          echo 'https://anchor.fm/s/216ede50/podcast/rss'
           curl https://anchor.fm/s/216ede50/podcast/rss --compressed > rss/%E3%81%9F%E3%81%BE%E3%83%87%E3%82%B6FM.xml
 
           # Submits.life
+          echo 'https://www.submits.life/feed.xml'
           curl https://www.submits.life/feed.xml --compressed > rss/submitslife.xml
 
           # Kyashfm
+          echo 'https://anchor.fm/s/4d007e0c/podcast/rss'
           curl https://anchor.fm/s/4d007e0c/podcast/rss --compressed > rss/kyashfm.xml
 
           # NeuroRadio
+          echo 'https://anchor.fm/s/4ddf5488/podcast/rss'
           curl https://anchor.fm/s/4ddf5488/podcast/rss --compressed > rss/NeuroRadio.xml
 
           # Background.radio
+          echo 'https://anchor.fm/s/4a05f894/podcast/rss'
           curl https://anchor.fm/s/4a05f894/podcast/rss --compressed > rss/bgradio.xml
 
           # ポッドキャストアワード観覧席
+          echo 'https://anchor.fm/s/471fb318/podcast/rss'
           curl https://anchor.fm/s/471fb318/podcast/rss --compressed > rss/%E3%83%9D%E3%83%83%E3%83%89%E3%82%AD%E3%83%A3%E3%82%B9%E3%83%88%E3%82%A2%E3%83%AF%E3%83%BC%E3%83%89%E8%A6%B3%E8%A6%A7%E5%B8%AD.xml
 
           # 研エンの仲
+          echo 'https://anchor.fm/s/2631f634/podcast/rss'
           curl https://anchor.fm/s/2631f634/podcast/rss --compressed > rss/%E7%A0%94%E3%82%A8%E3%83%B3%E3%81%AE%E4%BB%B2.xml
 
           # J-WAVE TOPPAN INNOVATION WORLD ERA
+          echo 'https://feeds.sonicbowl.cloud/rss/901233053125/5b61db6f-925a-42cb-b1bb-e54f185b19ab/'
           curl https://feeds.sonicbowl.cloud/rss/901233053125/5b61db6f-925a-42cb-b1bb-e54f185b19ab/ --compressed > rss/era813.xml
 
           # omoiyari.fm (lean-agile podcast)
+          echo 'http://feeds.feedburner.com/omoiyarifm/'
           curl http://feeds.feedburner.com/omoiyarifm/ --compressed > rss/omoiyarifm.xml
 
           # hikifune.fm
+          echo 'https://anchor.fm/s/8d50d4c/podcast/rss'
           curl https://anchor.fm/s/8d50d4c/podcast/rss --compressed > rss/hikifunefm.xml
 
           # 転職透明化ラジオ
+          echo 'https://anchor.fm/s/146371bc/podcast/rss'
           curl https://anchor.fm/s/146371bc/podcast/rss --compressed > rss/%E8%BB%A2%E8%81%B7%E9%80%8F%E6%98%8E%E5%8C%96%E3%83%A9%E3%82%B8%E3%82%AA.xml
 
           # mookjp のパソコンサタデーナイト
+          echo 'https://anchor.fm/s/328a780c/podcast/rss'
           curl https://anchor.fm/s/328a780c/podcast/rss --compressed > rss/%E3%83%91%E3%82%BD%E3%82%B3%E3%83%B3%E3%82%B5%E3%82%BF%E3%83%87%E3%83%BC%E3%83%8A%E3%82%A4%E3%83%88.xml
 
           # Today I Learned
+          echo 'https://anchor.fm/s/367f0040/podcast/rss'
           curl https://anchor.fm/s/367f0040/podcast/rss --compressed > rss/TodayILearned.xml
 
           # セキュリティのアレ
+          echo 'https://www.tsujileaks.com/?feed=rss2'
           curl https://www.tsujileaks.com/?feed=rss2 --compressed > rss/%E3%82%BB%E3%82%AD%E3%83%A5%E3%83%AA%E3%83%86%E3%82%A3%E3%81%AE%E3%82%A2%E3%83%AC.xml
 
           # 46fm
+          echo 'http://46fm.seike460.com/rss.xml'
           curl http://46fm.seike460.com/rss.xml --compressed > rss/46fm.xml
 
           # jamming.fm
+          echo 'https://anchor.fm/s/6c008db0/podcast/rss'
           curl https://anchor.fm/s/6c008db0/podcast/rss --compressed > rss/jammingfm.xml
 
           # texta.fm
+          echo 'https://anchor.fm/s/330a9488/podcast/rss'
           curl https://anchor.fm/s/330a9488/podcast/rss --compressed > rss/textafm.xml
 
           # Off Topic // オフトピック
+          echo 'https://anchor.fm/s/7369a14/podcast/rss'
           curl https://anchor.fm/s/7369a14/podcast/rss --compressed > rss/OffTopic.xml
 
           # Web Rush
+          echo 'https://feeds.simplecast.com/tOjNXec5'
           curl https://feeds.simplecast.com/tOjNXec5 --compressed > rss/webrush.xml
 
           # Secure Liaison
+          echo 'https://anchor.fm/s/2607ca1c/podcast/rss'
           curl https://anchor.fm/s/2607ca1c/podcast/rss --compressed > rss/secure%E6%97%85%E5%9B%A3.xml
 
           # AirSap
+          echo 'https://airsap.net/feed/podcast/'
           curl https://airsap.net/feed/podcast/ --compressed > rss/AirSap.xml
 
           # いんよう！
+          echo 'https://anchor.fm/s/576104c/podcast/rss'
           curl https://anchor.fm/s/576104c/podcast/rss --compressed > rss/%E3%81%84%E3%82%93%E3%82%88%E3%81%86.xml
 
           # 銀の弾丸ラジオ
+          echo 'https://anchor.fm/s/13df46f8/podcast/rss'
           curl https://anchor.fm/s/13df46f8/podcast/rss --compressed > rss/SilverBulletClub.xml
 
           # Yokohama North AM
+          echo 'https://anchor.fm/s/1e60bd50/podcast/rss'
           curl https://anchor.fm/s/1e60bd50/podcast/rss --compressed > rss/yokohamanortham.xml
 
           # FREE AGENDA by hikaru & yamotty
+          echo 'https://anchor.fm/s/147f7150/podcast/rss'
           curl https://anchor.fm/s/147f7150/podcast/rss --compressed > rss/%E3%83%95%E3%83%AA%E3%83%BC%E3%82%A2%E3%82%B8%E3%82%A7%E3%83%B3%E3%83%80.xml
 
           # 「話し方」のハナシ
+          echo 'https://hanashi.koelab.net/feed/podcast/'
           curl https://hanashi.koelab.net/feed/podcast/ --compressed > rss/%E8%A9%B1%E3%81%97%E6%96%B9%E3%81%AE%E3%83%8F%E3%83%8A%E3%82%B7.xml
 
           # ノウカノタネ
+          echo 'https://anchor.fm/s/1c8e7878/podcast/rss'
           curl https://anchor.fm/s/1c8e7878/podcast/rss --compressed > rss/%E3%83%8E%E3%82%A6%E3%82%AB%E3%83%8E%E3%82%BF%E3%83%8D.xml
+
+          # Researchat.fm
+          echo 'https://researchat.fm/feed.xml'
+          curl https://researchat.fm/feed.xml --compressed > rss/researchatfm.xml
+
+          # STILL RENDERING // スティレン
+          echo 'https://anchor.fm/s/40a74b4/podcast/rss'
+          curl https://anchor.fm/s/40a74b4/podcast/rss --compressed > rss/%E3%82%B9%E3%83%86%E3%82%A3%E3%83%AC%E3%83%B3.xml
+
+          # はなれより。
+          echo 'https://anchor.fm/s/c2e46e8/podcast/rss'
+          curl https://anchor.fm/s/c2e46e8/podcast/rss --compressed > rss/%E3%81%AF%E3%81%AA%E3%82%8C%E3%82%88%E3%82%8A.xml
+
+          # 中村繪里子・吉田尚記の本格雑談くちをひらく
+          echo 'https://www.omnycontent.com/d/playlist/67122501-9b17-4d77-84bd-a93d00dc791e/bf2b4e95-c669-4e1c-abcf-a98c00a5f513/b923e360-dc05-438d-be85-a98c00a5f517/podcast.rss'
+          curl https://www.omnycontent.com/d/playlist/67122501-9b17-4d77-84bd-a93d00dc791e/bf2b4e95-c669-4e1c-abcf-a98c00a5f513/b923e360-dc05-438d-be85-a98c00a5f517/podcast.rss --compressed > rss/%E3%81%8F%E3%81%A1%E3%82%92%E3%81%B2%E3%82%89%E3%81%8F.xml
+
