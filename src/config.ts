d230ddebb15605492c0c102176c003c9e30e7d2b
OkadaGenya <g.okada.wilf+github@gmail.com>
2022-11-27 00:21:15 +0900

    update config resize.fm

--- src/config.ts
+++ src/config.ts
@@ -526,6 +526,15 @@ export const urlList: UrlListItem[] = [
     hash: "MEDIATALK",
     url: "https://www.omnycontent.com/d/playlist/1e3bd144-9b57-451a-93cf-ac0e00e74446/50382bb4-3af3-4250-8ddc-ac0f0033ceb5/07a1de49-67cf-4714-8581-ac1000059302/podcast.rss",
   },
+
+  {
+    name: "resize.fm",
+    hash: "resizefm",
+    url: "https://anchor.fm/s/416f2048/podcast/rss",
+  },
+  
+  
+  
 ];
 
 /**
